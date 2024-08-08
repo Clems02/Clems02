b277e414daa09c9c4296df6f18bdaac966538586
Clems02 <chaumont.clement@outlook.fr>
2024-08-08 15:59:15 +0200

    edit readme

--- src/README_to_edit.ts
+++ src/README_to_edit.ts
@@ -33,10 +33,20 @@ export const README_to_edit = `
 
 <br/>
 
+<p align="center">
 [![Hits](https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2FClems02%2Fhit-counter&count_bg=%23003E80&title_bg=%235C9FE1&icon=powershell.svg&icon_color=%23FFFFFF&title=Visite&edge_flat=false)](https://hits.seeyoufarm.com)
+</p>
+
 
 <br/>
 
-![Badge](https://img.shields.io/badge/Last%20updated%20on-white?style=for-the-badge&logo=clockify)   ![Badge](https://img.shields.io/badge/<#lastUpdateDate>-white?style=for-the-badge) ![Badge](https://img.shields.io/badge/at-white?style=for-the-badge) ![Badge](https://img.shields.io/badge/<#lastUpdateHours>-white?style=for-the-badge)
 
+<p align="center">
+![Badge](https://img.shields.io/badge/Last%20updated%20on-white?style=for-the-badge&logo=clockify)   ![Badge](https://img.shields.io/badge/<#lastUpdateDate>-white?style=for-the-badge) ![Badge](https://img.shields.io/badge/at-white?style=for-the-badge) ![Badge](https://img.shields.io/badge/<#lastUpdateHours>-white?style=for-the-badge)
+</p>
+
+
+<p align="center">
+ <img width="1000" src="assets/github-snake.svg" alt="snake"/>
+</p>
 `;
